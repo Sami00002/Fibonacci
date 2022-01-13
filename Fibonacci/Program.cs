e6a2914dd9ef63c75344e7ec3ebaddb80a809daf
Sami00002 <samuel.bodogan@emanuel.ro>
2022-01-13 23:18:46 +0200

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un numar:");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(n));
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            else return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
